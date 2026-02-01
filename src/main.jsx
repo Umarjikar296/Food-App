2a0d4a082feb137cdb75c21c3195eff0b790bbe6
Ashwin Umarjikar <ashwin@Mac.fritz.box>
2026-02-01 22:22:44 +0100

    Fix store import path casing for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import Store from './redux/store.jsx';
+import Store from './Redux/store.jsx';
 import './index.css';
 import App from './App.jsx';
 
